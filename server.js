88894bd3f1877e3e5572b9164bd14cdbe790d31b
Jafaru (Ikechukwu) Abubakar <124154837+cyberjaffy@users.noreply.github.com>
2025-08-04 10:25:26 +0100

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000;
 
 // ✔️ Enable CORS (when you fix it)
 app.use(cors({
-  origin: 'https://YOUR_STATIC_WEB_APP.azurestaticapps.net'
+  origin: 'https://happy-desert-04dc06410.2.azurestaticapps.net'
 }));
 
 app.get('/api/hello', (req, res) => {
